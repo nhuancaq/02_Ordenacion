d0460c2dac5e8aa0d71365a3a8c48708030cc22d
nhuancaq <natalia.lizethqq@gmail.com>
2026-09-10 15:56:45 -0500

    Primera version: Agregando for

---
+++ Seleccion.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main(){
+    int numeros[] = {3,2,1,5,4};
+    int i,j,aux,min;
+    for(i=0;i<5;i++){
+        min = i;
+        for(j=i+1;j<5;j++){
+            if(numeros[j] < numeros[min]){
+                min = j;
+
+            }
+        }
+        aux = numeros[i];
+        numeros[i] = numeros[min];
+        numeros[min] = aux;
+    }
